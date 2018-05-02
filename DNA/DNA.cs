6b43f212104355468701cc25f31142d4dfed3114
Danelle Hennings <hennings@goldmail.etsu.edu>
2018-05-02 01:28:02 -0400

    OG 2016 code for project DNA, prior to any refactoring; simply for personal use and way to document how my coding style/habits have changed throughout my career.

---
+++ DNA/DNA.cs
@@ -0,0 +1,185 @@
+﻿// ---------------------------------------------------------------------------
+// File name: DNA.cs
+// Project name: DNA
+// ---------------------------------------------------------------------------
+// Creator’s name and email: Danelle Hennings hennings@goldmail.etsu.edu					
+// Creation Date: November 2, 2016		
+// ---------------------------------------------------------------------------
+using System;
+using System.IO;
+
+namespace DNA
+{
+    /// <summary>
+    /// Uses two text file objects to make comparison among their dna alleles, tallies up 
+    /// mismatches, calculates a percent of total match, returns info on the two dna files based
+    /// using this and the object properties
+    /// </summary>
+    class DNA
+    {
+        /// <summary>
+        /// Mains entry point for the program
+        /// </summary>
+        /// <param name="args">the program, text file1, text file 2</param>
+        static void Main(string[ ] args)
+        {
+            StreamReader sr1 = null,
+                         sr2 = null;
+            try
+            {
+                sr1 = new StreamReader(args[1]);     //open file
+                sr2 = new StreamReader(args[2]);     //open file
+            }
+            catch
+            {
+                Console.WriteLine("Unable to open one or both of the files: " + args[1] + ", " + args[2]);
+            }
+  
+            File f1 = new File();
+            File f2 = new File();
+            PullDNA(sr1, sr2, f1, f2);
+            Console.WriteLine(display(args, f1, f2));   //display DNA data
+            Console.ReadLine();
+
+            sr1.Close( );
+            sr2.Close( );
+        }
+
+        /// <summary>
+        /// pulls file in, reads one line, skips any comments or headers, assigns text to text object
+        /// </summary>
+        /// <param name="sr1">The stream for file1.</param>
+        /// <param name="sr2">The stream for file2.</param>
+        /// <param name="f1">The file 1.</param>
+        /// <param name="f2">The file 2param>
+        public static void PullDNA(StreamReader sr1, StreamReader sr2, File f1, File f2)
+        {
+            string text1,
+                    text2;        //one line of text from dna file
+            int genderCount1 = 0,   //counts the times "0 0" pair occurs in chromsome 24
+                genderCount2 = 0;   
+
+            //while each line of each files is not empty
+            while (( text1 = sr1.ReadLine( ) ) != null
+                && ( text2 = sr2.ReadLine( ) ) != null)
+            {   //FOR EACH BASE PAIR
+                //if line is a comment or column header, read in next line
+                while (text1.StartsWith("#") || text1.StartsWith("rsid"))
+                {   //read in next line of text from stream  
+                    text1 = sr1.ReadLine( );
+                }//repeat for next file, if 
+                while (text2.StartsWith("#") || text2.StartsWith("rsid"))
+                {   //read in next line of text from stream  
+                    text2 = sr2.ReadLine( );
+                }
+                //store elements from this line of text into dna elements array
+                f1.GetElements(text1);
+                f2.GetElements(text2);
+                //check only autosomes
+                if (int.Parse(f1.dna[1]) < 24)
+                {
+                    //if next line starts a new chromosome
+                    if (f1.chromosome != int.Parse(f1.dna[1]))
+                    {
+                        f1.Relation(f2);        //find relation among files
+                        f1.Next( );
+                        f2.Next( );
+                    }
+                    //increment for each new line (or set of basepairs)
+                    f1.numPairs++;
+                    f2.numPairs++;
+
+                    f1.Compare(f2);    //compare f1 against f2, increments for mismatch
+                    f2.Compare(f1);    //compare f2 against f1 
+                }
+                else
+                {
+                    if (int.Parse(f1.dna[1]) == 24)
+                    {
+                        // number of "0" genotypes found for each file
+                        if (f1.dna[3] == "0")
+                        {
+                            genderCount1++;
+                        }
+                        if (f2.dna[3] == "0")
+                        {
+                            genderCount2++;
+                        }
+                    }
+                    //set gender based on "0 0" pairs found in 24th chromosome
+                    f1.Gender(genderCount1);
+                    f2.Gender(genderCount2);
+                }
+            }//end while line not empty
+        }
+
+        /// <summary>
+        /// Displays information about DNA files, uses string[] args to refer
+        /// to the specific file names
+        /// </summary>
+        /// <param name="files">The specific file names as strings.</param>
+        /// <param name="f1">File 1</param>
+        /// <param name="f2">File 2</param>
+        /// <returns></returns>
+        public static string display(string[ ] files, File f1, File f2)
+        {
+            string text = "\n-------------------------------------------------------------\n";
+            text += string.Format("\n\t----{0} has a {1}%  match to {2}----      ", files[1].Substring(14, 4), Math.Round(calc(f1), 2), files[2].Substring(14, 4));
+            text += "\n-------------------------------------------------------------\n"
+                + string.Format("\t {0}: {1}", files[1].Substring(14, 4), f1.sex)
+                + "\t\t" + string.Format("{0}: {1}", files[2].Substring(14, 4), f2.sex)
+                + "\n-------------------------------------------------------------\n";
+            //98.9 is the threshhold for 2 files going from no relation to related
+            if (calc(f1) >= 98.9)
+            {  
+                if (f1.isChild == true)
+                {
+                    if (calc(f1) < 99)
+                    {
+                        text += string.Format("***------**{0} is the grandchild of {1}**------***", files[1].Substring(14, 4), files[2].Substring(14, 4));
+                    }
+                    else
+                        text += string.Format("****-------**{0} is the child of {1}**-------****", files[1].Substring(14, 4), files[2].Substring(14, 4));
+                }
+                else if (f2.isChild == true)
+                {
+                    if (calc(f2) < 99)
+                    {
+                        text += string.Format("***------**{0} is the grandchild of {1}**------***", files[2].Substring(14, 4), files[1].Substring(14, 4));
+                    }
+                    else
+                       text += string.Format("****-------**{0} is the child of {1}**-------****", files[2].Substring(14, 4), files[1].Substring(14, 4));
+                }
+                text += "\n-------------------------------------------------------------\n";
+            }
+            else
+            {
+                text += "There is not a parent-child relationship among these DNA files."
+                    + "\n-------------------------------------------------------------\n";
+            }
+            return text;
+        }
+
+        /// <summary>
+        /// Calculates percentage match against another file
+        /// </summary>
+        /// <param name="file">The file in comparison</param>
+        /// <returns>the percent</returns>
+        public static double calc(File file)
+        {
+            double sum = 0;
+            
+            foreach (double val in file.avgError)
+            {
+                sum += val;
+            }
+            sum = 100 - ( sum / 22 ) * 10;
+            return sum;
+        }   
+
+        
+
+
+
+    }
+}
---
+++ DNA/File.cs
@@ -0,0 +1,217 @@
+﻿// ---------------------------------------------------------------------------
+// File name: File.cs
+// Project name: DNA
+// ---------------------------------------------------------------------------
+// Creator’s name and email: Danelle Hennings hennings@goldmail.etsu.edu					
+// Creation Date: November 2, 2016		
+// ---------------------------------------------------------------------------
+namespace DNA
+{
+    /// <summary>
+    /// Used for retrieving any possible data from DNA files and turning this data into
+    /// relevant information on child-parent relationships and the sex of the dna
+    /// </summary>
+    class File
+    {
+        /// <summary>
+        /// Gets or sets the dna elements at each line
+        /// </summary>
+        /// <value>
+        /// array of 5 elements pertaining to columns in DNA file
+        /// </value>
+        public string[ ] dna
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Tracks the number of alelle 1 mismatches.
+        /// </summary>
+        /// <value>
+        /// The al1 mismatch.
+        /// </value>
+        public int al1Mismatch
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Tracks the number of alelle 2 mismatches.
+        /// </summary>
+        /// <value>
+        /// The al2 mismatch count.
+        /// </value>
+        public int al2Mismatch
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is child.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is child; otherwise, <c>false</c>.
+        /// </value>
+        public bool isChild
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Gets or sets the current chromosome
+        /// </summary>
+        /// <value>
+        /// The current chromosome.
+        /// </value>
+        public int chromosome
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Gets or sets the number for tracking the base pair count for current chromosme
+        /// </summary>
+        /// <value>
+        /// The number of base pairs.
+        /// </value>
+        public int numPairs
+        {
+            get; set;
+        } 
+        /// <summary>
+        /// Gets or sets the gender of the DNA file.
+        /// </summary>
+        /// <value>
+        /// string containing "male" or "female" depending on determined sex
+        /// </value>
+        public string sex
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Tracks the average number of mismatches for each chromsome
+        /// </summary>
+        /// <value>
+        /// array of 22 averages
+        /// </value>
+        public double[ ] avgError
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Default constructor for creating new instances of the File class.
+        /// </summary>
+        public File()
+        {
+            dna = new string[5];       //initialize string array of 5 elements regarding current line in dna
+            al1Mismatch = 0;
+            chromosome = 1;             //initalize chromosome to 1
+            numPairs = 0;               //initialize number of base pairs to be incrememented for each line in a chromosome
+            al2Mismatch = 0;
+            sex = "undetermined";       //starts with an unknown sex for the DNA
+            isChild = false;           //assume not the child file
+            avgError = new double[22];
+        }//end file
+
+        /// <summary>
+        /// Gets the elements from the line in the file by uisng whitespace 
+        /// to split into a string array of the 5 elements .
+        /// </summary>
+        /// <param name="line">The line.</param>
+        public void GetElements(string line)
+        {
+            //split up the 5 elements from this line into an array
+            dna = line.Split( );  
+        }
+
+        /// <summary>
+        /// Compares the current line of this file against the one passed and counts the number of mismatches
+        /// found for each allele
+        /// </summary>
+        /// <param name="file">The file this one is compared against</param>
+        public void Compare(File file)
+        {
+            /**First DNA allelle 1 compared to all of Second DNA**/
+            //if 1st allele of 1st dna does not equal either alleles of 2nd dna
+            if (dna[3] != file.dna[3] && dna[3] != file.dna[4])
+            {
+                al1Mismatch++;   //increment # of mismatches for allele 1
+            }
+            //if 2nd allele of 1st dna does not equal either alleles of 2nd dna
+            if (dna[4] != file.dna[3] && dna[4] != file.dna[4])
+            {
+                al2Mismatch++;   //increment # of mismatches for allele 2
+            }
+        }
+
+        /// <summary>
+        /// When switching to the next chromosome this calculates an average for the
+        /// number of mismatches out of the number of base pairs for chromsome
+        /// It resets the number of mismatches and numPairs and increments to the next chromosome
+        /// </summary>
+        public void Next( )
+        {  
+            if (al1Mismatch < al2Mismatch)
+            {
+                avgError[chromosome - 1] = al1Mismatch / (double)numPairs; 
+            }
+            else
+            {
+                avgError[chromosome - 1] = al2Mismatch / (double)numPairs;
+            }
+
+            al1Mismatch = 0;
+            al2Mismatch = 0;
+            numPairs = 0;
+            chromosome++;
+        }//end Next
+
+        /// <summary>
+        /// Changes the isChild boolean value to true for the object if the total
+        /// number of allele mismatches is the least among all four comparisons
+        /// at each chromosome
+        /// </summary>
+        /// <param name="file">The file being compared against</param>
+        public void Relation(File file)
+        {
+            int least = int.MaxValue;       //start with least being largest possible integer   
+
+            if (al1Mismatch < least)
+            {
+                least = al1Mismatch;
+                isChild = true;
+            }
+            else if (al2Mismatch < least)
+            {
+                least = al2Mismatch;
+                isChild = true;
+            }
+            else if (file.al1Mismatch < least)
+            {
+                least = file.al1Mismatch;
+                file.isChild = true;
+            }
+            else 
+            {
+                least = file.al2Mismatch;
+                file.isChild = true;
+            }
+                   
+        }
+        /// <summary>
+        /// Uses 24th chromosome to determine sex of DNA, instead of calculating average of mismatches
+        /// on this chromosome, it is used to count number of "0 0" genotypes,
+        /// when this method is called it uses this count to verify there are enough of these pairs to determine
+        /// it is a male DNA file, and if not then it is female
+        /// sets gender property to "male" or "female"
+        /// </summary>
+        /// <returns>"male" or "female"</returns>
+        public void Gender(int numPairs)
+        {   
+            //if large enough count of 00 pairs
+            if (numPairs > 100)
+            {   //then male
+                sex = "male";
+            }
+            else
+                sex = "female";
+        }  
+    }//end class file
+
+
+}//end namespace DNA
